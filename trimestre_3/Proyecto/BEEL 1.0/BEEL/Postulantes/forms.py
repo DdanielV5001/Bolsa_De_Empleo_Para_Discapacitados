6c26e8ebe403e9b63aebbbc502eeb3e9ead6c882
Cotangente <julianavila458@gmail.com>
2024-05-31 15:48:39 -0500

    Actualización 1.2 31/05/2024 requeriments.txt, botón de regresar a la raíz, seguridad de contraseñas en las views.py, confirmación de contraseña en los forms.py

--- trimestre_3/Proyecto/BEEL 1.0/BEEL/Postulantes/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import postulante
-
-
-class postulanteForm(forms.ModelForm):
-    class Meta:
-        model = postulante
-        fields = '__all__'
--- trimestre_3/Proyecto/BEEL 1.1/BEEL/Sistema_autenticacion_registro/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SistemaAutenticacionRegistroConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Sistema_autenticacion_registro'
